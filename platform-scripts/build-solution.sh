6c501a6f6cac72338ac56cae0c8c7f8028b66478
Alan Scott <alscott@redhat.com>
2020-05-05 16:02:47 -0500

    Update build-solutions.sh script

--- platform-scripts/build-solution.sh
+++ platform-scripts/build-solution.sh
@@ -1,10 +1,8 @@
 # Change Directory to solution on local machine
-_mydir="/Users/alscott/Development/IntelliJ/OpenSource-iDAAS/iDAAS-Connect-Clinical-IndustryStandards/"
-cd _mydir
-echo "The current directory: $_mydir"
-# Run Maven Build
-mvn clean install
+echo "iDAAS - Connect Clinical Industry Standards"
+cd /Users/alscott/Development/IntelliJ/OpenSource-iDAAS/iDAAS-Connect-Clinical-IndustryStandards/
+
+/usr/local/bin/mvn clean install
 echo "Maven Build Completed"
-# Run Maven Release
-mvn package
+/usr/local/bin/mvn package
 echo "Maven Release Completed"
